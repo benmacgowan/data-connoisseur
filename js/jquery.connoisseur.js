b7be0a1cf4d88d2ca2477d044e98f0ad8fa1e815
Ben MacGowan <benmacgowan@Bens-MacBook-Pro.local>
2014-05-21 17:42:50 +0100

    Adding ifCond handlebar helper

--- js/jquery.connoisseur.js
+++ js/jquery.connoisseur.js
@@ -219,3 +219,26 @@ if (typeof Object.create !== 'function') {
         return this;
     }
 })(jQuery);
+
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
